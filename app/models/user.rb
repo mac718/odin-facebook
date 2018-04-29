938d339505f405b666fc4bf1ff4e1aec6de7381a
mac718 <mac718@gmail.com>
2018-04-29 11:31:24 -0400

    Refine the friending/friend request funtionality further; add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
   has_many :friend_requests, dependent: :destroy
   has_many :pending_friends, :through => :friend_requests, :source => :friend
+  has_many :inverse_friend_requests, class_name: 'FriendRequest', foreign_key: 'friend_id'
+  has_many :inverse_pending_friends, :through => :inverse_friend_requests, :source => 'user'
 
   def remove_friend(friend)
     current_user.friends.destroy(friend)
