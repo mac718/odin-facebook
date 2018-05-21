02ffa3ee9a653f0ad6753cfb6a7293561983e469
mac718 <mac718@gmail.com>
2018-05-21 19:08:37 -0400

    Add first_name and last_name to devise_parameter_sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
-  #protect_from_forgery prepend: true
-  #protect_from_forgery prepend: true
-  #protect_from_forgery prepend: true, with: :exception
-  #skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :first_name, :last_name])
   end
 end
