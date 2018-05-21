0dd65af99f67acf2ee22629076a4b5b2cf42d5c3
mac718 <mac718@gmail.com>
2018-05-21 18:45:26 -0400

    Add first_name, last_name, and image columns to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,9 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.profile.first_name = auth.info.first_name   # assuming the user model has a name
-      user.profile.last_name = auth.info.last_name   # assuming the user model has a name
-      user.profile.avatar = auth.info.image # assuming the user model has an image
+      user.first_name = auth.info.first_name   # assuming the user model has a name
+      user.last_name = auth.info.last_name   # assuming the user model has a name
+      #user.avatar = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515021211) do
+ActiveRecord::Schema.define(version: 20180521224237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180515021211) do
     t.string   "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json     "photos"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,9 +82,11 @@ ActiveRecord::Schema.define(version: 20180515021211) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
