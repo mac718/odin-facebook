04c9ab2f65b06aca4fc055bc0e2031bab3de3fb8
mac718 <mac718@gmail.com>
2018-05-24 18:49:26 -0400

    Add profile creation to User.from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,17 +36,20 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      full_name = auth.info.name.split
-      user.first_name = full_name[0]   # assuming the user model has a name
-      user.last_name = full_name[1]   # assuming the user model has a name
-      user.avatar = auth.info.image # assuming the user model has an image
+      
+      # assuming the user model has a name
+      # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
+      user
     end
+    full_name = auth.info.name.split
+    profile = user.profile.build(first_name: full_name[0], last_name: [1], avatar: auth.info.image)
+    profile.save
   end
 
    def self.new_with_session(params, session)
