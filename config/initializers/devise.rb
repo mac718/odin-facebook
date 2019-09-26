893c2e39514473f1e705e12f5cc81130a46ea00d
Michael Coon <mac718@gmail.com>
2019-09-26 13:54:25 -0700

    removed duplicate oauth config in omniauth.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV["app_ID"], ENV["app_secret"]
+  config.omniauth :facebook, Figaro.env.app_ID, Figaro.env.app_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  secure_image_url: true
-end
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :facebook,ENV['app_ID'], ENV['app_secret'], provider_ignores_state: true,
+  #client_options: {
+  #    site: 'https://graph.facebook.com/v3.0',  # this is the example API version
+  #    authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+  #  },#ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+#  secure_image_url: true
+#end
